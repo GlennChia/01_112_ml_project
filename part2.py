1ee12dbe5318144741030484b50e41508df42da1
Eda <edathr2@gmail.com>
2019-11-20 12:44:54 +0800

    Added smoothing for test
    - Wrote function for reading test files
    - Wrote function for test smoothing

--- part2.py
+++ part2.py
@@ -2,36 +2,60 @@ import pandas as pd
 
 en_path = 'EN/train'
 
-# def clean_data(data):
 
-def read_to_pdf(file_path):
+def readtopdftrain(file_path):
     with open(file_path) as f_message:
         temp = f_message.read().splitlines()
         temp = list(filter(None, temp))
     separated_word_tags = [word_tags.split(' ') for word_tags in temp]
-    df = pd.DataFrame(separated_word_tags, columns =['words', 'tags'])
+    df = pd.DataFrame(separated_word_tags, columns=['words', 'tags'])
     return df
 
 
-def replaceword(word, word_counts, k):
+def readtopdftest(file_path):
+    with open(file_path) as f_message:
+        temp = f_message.read().splitlines()
+        temp = list(filter(None, temp))
+    df = pd.DataFrame(temp, columns=['words'])
+    return df
+
+
+def replacewordtrain(word, word_counts, k):
     if word_counts[word] < k:
         return "#UNK#"
     return word
 
-def smoothing(data, k=3):
+
+def smoothingtrain(data, k=3):
     word_counts = data['words'].value_counts().to_dict()
-    data['words'] = data['words'].apply(lambda word: replaceword(word, word_counts, k))
+    data['words'] = data['words'].apply(lambda word: replacewordtrain(word, word_counts, k))
     return data
 
 
+def smoothingtest(testdata, traindata):
+    trainvalues = set(traindata['words'])
+    testdata['words'] = testdata['words'].apply(lambda word: replacewordtest(word, trainvalues))
+    return testdata
+
+
+def replacewordtest(word, train):
+    if word in train:
+        return word
+    return "#UNK#"
+
+
 def estimate_emission_parameters(word, tag, df):
-    count_x_given_y = df[df['tags']==tag].words.str.count(word).sum()
+    count_x_given_y = df[df['tags'] == tag].words.str.count(word).sum()
     count_y = df.tags.str.count(tag).sum()
     return count_x_given_y/count_y
 
 
 if __name__=="__main__":
     '''Part 2 Qn 1: Test MLE'''
-    df = read_to_pdf(en_path)
-    df = smoothing(df)
-    print(estimate_emission_parameters('stress-related', 'B-NP', df))
+    traindf = readtopdftrain(en_path)
+    smoothedtrain = smoothingtrain(traindf)
+    testfilepath = 'EN/dev.in'
+    testdf = readtopdftest(testfilepath)
+    smoothedtest = smoothingtest(testdf, smoothedtrain)
+    print(estimate_emission_parameters('stress-related', 'B-NP', smoothedtrain))
+    print(smoothedtest['words'].value_counts())
